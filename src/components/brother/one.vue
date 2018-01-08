f1c7decbf3d4d6dac24cec0521388201a38bbd74
柴米油盐 <471097828@qq.com>
2018-01-08 11:35:45 +0800

    liusu0913
    利用store的getters进行取数据操作的数据筛选，灾区数据时也通过store的getters来获取筛选的数据

--- src/components/brother/one.vue
+++ src/components/brother/one.vue
@@ -2,6 +2,10 @@
 	<div class="one">
 		<h1>我是兄弟1</h1>
 		<p>count的数据为{{count}}</p>
+		<p>利用store的getters筛选获取到的foods</p>
+		<ul>
+			<li v-for="food in foods">{{food.name}}</li>
+		</ul>
 		<button @click="addOne">点击按钮加1</button>
 	</div>
 </template>
@@ -12,6 +16,11 @@
 		computed: {
 			count() {
 				return this.$store.state.count;
+			},
+			// food的数据的获取，利用store的getters筛选获取
+			foods() {
+				console.log(this.$store.getters.filterFuite);
+				return this.$store.getters.filterFuite;
 			}
 		},
 		methods: {
--- src/main.js
+++ src/main.js
@@ -12,7 +12,27 @@ Vue.use(vueResource)
 // // 声明vuex的公共变量
 const store = new Vuex.Store({
 	state: {
-		count: 0
+		count: 0,
+		foods: [{
+			name: '香蕉',
+			isShow: true
+		},{
+			name: '苹果',
+			isShow: true
+		},{
+			name: '葡萄',
+			isShow: true
+		},{
+			name: '大米',
+			isShow: false
+		}]
+	},
+	// 创建那数据的时候的筛选函数，用来过滤foods中的不需要的数据
+	getters: {
+		filterFuite(state) {
+			return state.foods.filter(item => item.isShow);
+			// 此处的写法等于(item)=>{return item.isShow}
+		}
 	},
 	mutations: {
 		add(state,obj) {
